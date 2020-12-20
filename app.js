23222a64d4447aa029983628a851af8af1379281
Jackson Hsieh <jackson97042@gmail.com>
2020-12-20 15:06:34 +0800

    feat: add hbs UI

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-const express = required('express')
+const express = require('express')
+const exhbs = require('express-handlebars')
+const User = require('./models/user')
+require('./config/mongoose')
+
 const app = express()
 const port = 3000
 
+app.engine('hbs', exhbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+
 
 app.get('/', (req, res) => {
-    res.send('Hi, Alpha Camp.')
+    res.render('index')
 })
-
 
 app.listen(port, () => {
     console.log(`This server is listening to http://localhost:${port}`)
