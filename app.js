92d6da0140e6b3679b7c3dc79d9927df2f83ae93
Jackson Hsieh <jackson97042@gmail.com>
2020-12-20 12:05:32 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = required('express')
+const app = express()
+const port = 3000
+
+
+app.get('/', (req, res) => {
+    res.send('Hi, Alpha Camp')
+})
+
+
+app.listen(port, () => {
+    console.log(`This server is listening to http://localhost:${port}`)
+})
