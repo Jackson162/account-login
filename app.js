21cba6a1c24ebbf2476d49a632e4f74ac25caef0
Jackson Hsieh <jackson97042@gmail.com>
2020-12-20 16:28:14 +0800

    feat: check person submit log in

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exhbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const User = require('./models/user')
 require('./config/mongoose')
 
@@ -9,11 +10,45 @@ const port = 3000
 app.engine('hbs', exhbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
 
+let personLogin = undefined
+console.log('reassign personLogin')
+
+app.get('/login', (req, res) => {
+    if (!personLogin) {
+        res.render('login')
+    } else {
+        res.redirect('/')
+    }
+})
+
+app.post('/login', (req, res) => {
+    const logIn = req.body
+    User.find()
+        .lean()
+        .then(users => {
+            personLogin = users.find(user => logIn.email === user.email && logIn.password === user.password)
+            if (personLogin) {
+                return res.redirect('/')  
+            } else {
+                return res.send('User doesn\'t exist.')
+            }
+        })
+        .catch(error => console.log(error))
+})
 
 app.get('/', (req, res) => {
-    res.render('index')
+    if (personLogin) {
+        console.log('no reassign')
+        return res.send(`Welcome! ${personLogin.firstName}`)
+    } else {
+        console.log('user is undeifned')
+        return res.redirect('/login')
+    }    
 })
+
+
 
 app.listen(port, () => {
     console.log(`This server is listening to http://localhost:${port}`)
